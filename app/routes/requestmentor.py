8b165b15c8ab1d52aa38950ddbebf0975aac9742
Jobayer Hossen <jobayerhossen64@gmail.com>
2023-01-16 22:55:18 +0600

    updated

--- app/routes/requestmentor.py
+++ app/routes/requestmentor.py
@@ -32,7 +32,7 @@ def request_mentor(s):
             # email = ['email']
             print(y)
 
-        msg = Message('want to be hired as a mentor? ', sender='meetyourmentor150@gmail.com', recipients=[maill])
+        msg = Message('Want to be hired as a mentor? ', sender='meetyourmentor150@gmail.com', recipients=[maill])
         msg.body = "Message from requester for you: " + message + "\n""Requested by  " + name + "\n" "For Details Contact with " + email + " And Phone Number: " + number
         mail.send(msg)
         post = {'ment_username': ment_username, 'std_username': username, 'std_name': name, 'email' :email, 'message':message, 'current_time':datetime.now().strftime("%d/%m/%Y %H:%M:%S") }
