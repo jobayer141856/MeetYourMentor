fd917b7663c0f21b6cfc37a1292cad51feb7939c
Jobayer Hossen <jobayerhossen64@gmail.com>
2023-01-16 20:16:21 +0600

    updated

--- app/routes/mentorprofile.py
+++ app/routes/mentorprofile.py
@@ -1,5 +1,5 @@
 from app import*
-
+from bson.objectid import ObjectId
 app.config['UPLOAD_FOLDER'] = '../static/images/'
 app.config['SECRET_KEY'] = 'supersecretkey'
 
@@ -40,5 +40,31 @@ def mentorprofile():
 
 @app.route('/mentordash', methods=['GET','POST'])
 def mentordash():
-    
+    std_name = []
+    std_email = []
+    std_msg = []
+    current_time = []
+    id = []
+    cnt = 0
+    username = session['username']
+    for z in db_request_mentor.find():
+        if username == z['std_username']:
+            std_name.append(z["std_name"])
+            std_email.append(z["email"])
+            current_time.append(z["current_time"])
+            std_msg.append(z["message"])
+            id.append(z["_id"])
+            cnt += 1
     return render_template("mentordash.html", **locals())
+
+
+@app.route('/delete_ment/<string:s>',  methods=['GET', 'POST'])
+def delete_ment(s):
+    if "username" in session:
+        username = session["username"]
+        s = str(s)
+        print("Object id " +s)
+        db_request_student.delete_many({'_id': ObjectId(s)})
+        return redirect(url_for('mentordash'))
+
+    return render_template("mentordash.html", **locals()) 
